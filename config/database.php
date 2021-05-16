6a5fd4c36ab05b94bd217cf8a18e27819102b899
hiroka <hrk.ismr.419forprgrm@gmail.com>
2021-05-16 17:29:14 +0900

    change files for circleci

--- config/database.php
+++ config/database.php
@@ -81,21 +81,6 @@ return [
             ]) : [],
         ],
 
-        'circle_test' => [
-            'driver' => 'mysql',
-            'host' => '127.0.0.1',
-            'port' => '3306',
-            'database' => 'app_test',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-
-
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
