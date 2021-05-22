8194cf41529a3fcb9b6c594a752aeada416b1ace
hiroka <hrk.ismr.419forprgrm@gmail.com>
2021-05-22 21:24:48 +0900

    fix css environment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/style.css">
+        @if(app('env')=='local')
+        <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+        @endif
+        @if(app('env')=='production')
+        <link rel="stylesheet" href="{{ secure_asset('/css/style.css') }}">
+        @endif
     </head>
 
     <body>
