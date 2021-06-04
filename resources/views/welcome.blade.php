c30dd87835461f5be1fff26a5108c03b446e0c3a
hiroka <hrk.ismr.419forprgrm@gmail.com>
2021-06-04 19:11:34 +0900

    change h1 sentence and font size for mobile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         </div>
     @else
         <div class="text-center m-auto top-page">
-            <h1 class="headcontent p-4 m-auto" style="font-size:6vw; font-weight:540;">Welcome to SaunaShare</h1>
+            <h1 class="headcontent p-4 m-auto" style="font-size:6vw; font-weight:540;">SaunaShare</h1>
             <p class="pt-4 px-4" style="line-height:4rem; font-size:2.5vh">
                 SaunaShareは無料で始められて、あなたが行ったサウナを写真と共にシェアできます。
                 このコミュニティを通して、新しいサウナ仲間ができるかもしれません。
@@ -31,7 +31,7 @@
                 </div>
             </div>
             <div class="pb-5">
-                <p style="font-size:20px;">＊採用担当者様はゲストログインへお進みください。
+                <p style="font-size:2.5vh;">＊採用担当者様はゲストログインへお進みください。
                 あらかじめセットされているログイン情報でログイン可能となっております。</p>
             </div>
         </div>
