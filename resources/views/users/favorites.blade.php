eb8f37c18153290c776fe36538dc2f9407d62102
hiroka <hrk.ismr.419forprgrm@gmail.com>
2021-06-09 01:13:06 +0900

    change favorites followings followers files

--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <aside class="col-sm-3">
-            {{-- ユーザ情報 --}}
-            @include('users.card')
-        </aside>
-        <div class="col-sm-9">
-            {{-- タブ --}}
-            @include('users.navtabs')
-            {{-- ユーザ一覧 --}}
-            @include('posts.posts')
-        </div>
+<div class="row m-auto">
+    <div class="col-sm-10 offset-sm-1">
+        <!-- ユーザ情報 -->
+        @include('users.card')
+        <!-- タブ -->
+        @include('users.navtabs')
+        <!-- お気に入り投稿一覧 -->
+        @include('posts.posts')
     </div>
+</div>
 @endsection
